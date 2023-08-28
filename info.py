2bb5f67003327fc70cbced191e9292cd4cd5e05a
htmlboss123 <91206261+htmlboss123@users.noreply.github.com>
2023-08-28 14:54:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,8 +13,8 @@ def is_enabled(value, default):
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '21956488'))
-API_HASH = int(environ.get('API_HASH', '812529f879f06436925c7d62eb49f5d1'))
-BOT_TOKEN = int(environ.get('BOT_TOKEN', '6481049454:AAHpuh61OXk12sSG6E-hW_OMghxdypx6e6k'))
+API_HASH = environ.get('API_HASH', '812529f879f06436925c7d62eb49f5d1'))
+BOT_TOKEN = environ.get('BOT_TOKEN', '6481049454:AAHpuh61OXk12sSG6E-hW_OMghxdypx6e6k'))
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
