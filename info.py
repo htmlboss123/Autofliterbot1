f2f83bcbad5be362a816d93501eb4df2cb9b0420
htmlboss123 <91206261+htmlboss123@users.noreply.github.com>
2023-08-28 13:20:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,9 +11,9 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-SESSION = SESSION = environ.get('SESSION', 'Media_search')
+SESSION = SESSION = environ.get('SESSION', 'Media_search'))
 API_ID = int(environ.get('API_ID', '21956488'))
-API_HASH = environ.get('API_HASH', '812529f879f06436925c7d62eb49f5d1')
+API_HASH = environ.get('API_HASH', '812529f879f06436925c7d62eb49f5d1'))
 BOT_TOKEN = environ.get('BOT_TOKEN', '6481049454:AAHpuh61OXk12sSG6E-hW_OMghxdypx6e6k'))
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
